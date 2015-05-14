4fdb7d0cf3918da0c0bd31b764f2848e22e4dcc8
Jören Haag <joeren.haag@gmail.com>
2015-05-14 22:52:10 +0200

    changed hash prev block/merke root

--- educoins-core/src/main/java/org/educoins/core/GenesisBlock.java
+++ educoins-core/src/main/java/org/educoins/core/GenesisBlock.java
@@ -3,8 +3,8 @@ package org.educoins.core;
 public final class GenesisBlock extends Block {
 
 	private static final int VERSION = 2;
-	private static final String HASH_PREV_BLOCK = "0000000000000000000000000";
-	private static final String HASH_MERKLE_ROOT = "0000000000000000000000000";
+	private static final String HASH_PREV_BLOCK = "0000000000000000000000000000000000000000000000000000000000000000";
+	private static final String HASH_MERKLE_ROOT = "0000000000000000000000000000000000000000000000000000000000000000";
 	private static final long TIME = System.currentTimeMillis();
 	private static final String BITS = "1e01ff3f";
 	private static final long NONCE = 1114735442;
