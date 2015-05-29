e0acc9560a008b2ae80bce2d2e4467ba4efd22a7
Joeren Haag <joeren.haag@gmail.com>
2015-05-29 16:14:34 +0200

    made some modifications and Client class

---
+++ educoins-core/src/main/java/org/educoins/core/Client.java
@@ -0,0 +1,5 @@
+package org.educoins.core;
+
+public class Client {
+
+}
--- educoins-core/src/main/java/org/educoins/core/utils/Serializer.java
+++ educoins-core/src/main/java/org/educoins/core/utils/Serializer.java
@@ -3,10 +3,11 @@ package org.educoins.core.utils;
 import org.educoins.core.Block;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public abstract class Serializer {
 
-	private static final Gson GSON_SERIALIZER = new Gson();
+	private static final Gson GSON_SERIALIZER = new GsonBuilder().setPrettyPrinting().create();
 
 	public static String serialize(Block block) {
 		try {
