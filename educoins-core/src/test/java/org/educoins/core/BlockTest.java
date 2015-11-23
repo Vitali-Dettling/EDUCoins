4d8cc3668fae51f4b8ab647e04b64d299696b0a7
Michael Kreusel <m.Kreusel@gmail.com>
2015-11-23 13:58:38 +0100

    fixed duplicate code in BlockTest.java

--- educoins-core/src/test/java/org/educoins/core/BlockTest.java
+++ educoins-core/src/test/java/org/educoins/core/BlockTest.java
@@ -16,70 +16,63 @@ public class BlockTest{
     public void BitsSetterTest() {
         byte[] input = ByteArray.convertFromString("ffffffffffffffffffffffffffffffff"); //32
         byte[] expectedCompact = ByteArray.convertFromString("0dffffff");
-        Block b = new Block();
-        b.setBits(Sha256Hash.wrap(input));
-        try {
-            Field f = b.getClass().getDeclaredField("bits"); //NoSuchFieldException
-            f.setAccessible(true);
-            byte[] compactBits = (byte[]) f.get(b);
-            Assert.assertArrayEquals(expectedCompact, compactBits);
-        } catch (NoSuchFieldException | IllegalAccessException ex) {
-            Assert.fail();
-        }
+        Assert.assertArrayEquals(expectedCompact, getCompactBits(input));
     }
 
     @Test
     public void BitsSetter64Test() {
         byte[] input = ByteArray.convertFromString("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); //64
         byte[] expectedCompact = ByteArray.convertFromString("1dffffff");
+        Assert.assertArrayEquals(expectedCompact, getCompactBits(input));
+    }
+
+    private byte[] getCompactBits(byte[] input) {
         Block b = new Block();
         b.setBits(Sha256Hash.wrap(input));
+        byte[] compactBits = null;
         try {
             Field f = b.getClass().getDeclaredField("bits"); //NoSuchFieldException
             f.setAccessible(true);
-            byte[] compactBits = (byte[]) f.get(b);
-            Assert.assertArrayEquals(expectedCompact, compactBits);
+            compactBits = (byte[]) f.get(b);
         }
         catch (NoSuchFieldException | IllegalAccessException ex){
             Assert.fail();
         }
+        return compactBits;
     }
 
     @Test
     public void BitsGetterTest(){
         byte[] input = ByteArray.convertFromString("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         byte[] expec = ByteArray.convertFromString("ffffff0000000000000000000000000000000000000000000000000000000000");
-        Block b = new Block();
-        b.setBits(Sha256Hash.wrap(input));
-        Assert.assertArrayEquals(expec, b.getBits().getBytes());
+        Assert.assertArrayEquals(expec, getBits(input));
     }
 
     @Test
     public void BitsGetter2Test(){
         byte[] input = ByteArray.convertFromString("7420dffffffffff");
         byte[] expec = ByteArray.convertFromString("7420d0000000000");
-        Block b = new Block();
-        b.setBits(Sha256Hash.wrap(input));
-        byte[] bits = b.getBits().getBytes();
-        Assert.assertArrayEquals(expec, bits);
+        Assert.assertArrayEquals(expec, getBits(input));
     }
 
     @Test
     public void BitsGetter3Test(){
         byte[] input = ByteArray.convertFromString("fffffffffffffffffff");
         byte[] expec = ByteArray.convertFromString("fffff00000000000000");
-        Block b = new Block();
-        b.setBits(Sha256Hash.wrap(input));
-        Assert.assertArrayEquals(expec, b.getBits().getBytes());
+        Assert.assertArrayEquals(expec, getBits(input));
     }
     
     @Test
     public void BitsGetter4Test(){
         byte[] input = ByteArray.convertFromString("1bc330123456789abcde");
         byte[] expec = ByteArray.convertFromString("1bc33000000000000000");
+        Assert.assertArrayEquals(expec, getBits(input));
+    }
+
+    private byte[] getBits(byte[] input) {
         Block b = new Block();
         b.setBits(Sha256Hash.wrap(input));
-        Assert.assertArrayEquals(expec, b.getBits().getBytes());
+        return b.getBits().getBytes();
     }
 
     @Test
