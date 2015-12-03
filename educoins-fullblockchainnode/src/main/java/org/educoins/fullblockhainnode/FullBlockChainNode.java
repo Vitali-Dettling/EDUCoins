cde52768b81f8a447edd71e853639e4b0c388b51
typusOmega <licksalt@gmx.net>
2015-12-03 15:02:59 +0100

    + Added new Projects for fullblockchain and reference node

---
+++ educoins-fullblockchainnode/src/main/java/org/educoins/fullblockhainnode/FullBlockChainNode.java
@@ -0,0 +1,42 @@
+package org.educoins.fullblockhainnode;
+
+import org.educoins.core.Block;
+import org.educoins.core.BlockChain;
+import org.educoins.core.p2p.discovery.DiscoveryException;
+import org.educoins.core.p2p.peers.FullBlockChainPeer;
+import org.educoins.core.p2p.peers.HttpProxyPeerGroup;
+import org.educoins.core.store.BlockStoreException;
+import org.educoins.core.store.LevelDbBlockStore;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * The HttpServer serving {@link Block}s.
+ * Manages all ingoing requests to the REST-API.
+ * Created by typus on 11/5/15.
+ */
+@SpringBootApplication
+@EnableWebMvc
+@EnableAutoConfiguration(exclude = {JacksonAutoConfiguration.class})
+@ComponentScan(basePackages = "org.educoins.core")
+public class FullBlockChainNode {
+
+    private FullBlockChainPeer peer;
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullBlockChainNode.class, args);
+    }
+
+    @PostConstruct
+    private void start() throws BlockStoreException, DiscoveryException {
+        HttpProxyPeerGroup peerGroup = new HttpProxyPeerGroup();
+        peer = new FullBlockChainPeer(new BlockChain(peerGroup, peerGroup, peerGroup, new LevelDbBlockStore()));
+        peer.start();
+    }
+}
