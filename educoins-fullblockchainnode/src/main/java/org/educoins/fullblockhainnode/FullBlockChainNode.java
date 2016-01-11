d9622f8178fc49fc6fc8d79203f15b7d1c549704
Julian <julian_schaefer93@web.de>
2016-01-11 12:08:50 +0100

    Fixed imports and issues with maven install

--- educoins-fullblockchainnode/src/main/java/org/educoins/fullblockhainnode/FullBlockChainNode.java
+++ educoins-fullblockchainnode/src/main/java/org/educoins/fullblockhainnode/FullBlockChainNode.java
@@ -1,32 +1,33 @@
-//package org.educoins.fullblockhainnode;
-//
-//import org.educoins.core.Block;
-//import org.educoins.core.BlockChain;
-//import org.educoins.core.p2p.discovery.DiscoveryException;
+
+package org.educoins.fullblockhainnode;
+
+import org.educoins.core.Block;
+import org.educoins.core.BlockChain;
+import org.educoins.core.p2p.discovery.DiscoveryException;
 //import org.educoins.core.p2p.peers.FullBlockChainPeer;
-//import org.educoins.core.store.BlockStoreException;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.ConfigurableApplicationContext;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//
-///**
-// * The HttpServer serving {@link Block}s.
-// * Manages all ingoing requests to the REST-API.
-// * Created by typus on 11/5/15.
-// */
-//@SpringBootApplication
-//@EnableWebMvc
-//@EnableAutoConfiguration(exclude = {JacksonAutoConfiguration.class})
-//@ComponentScan(basePackages = "org.educoins.core")
-//@ConfigurationProperties(value = "classpath:/application.properties")
-//public class FullBlockChainNode {
-//
-//    public static void main(String[] args) throws BlockStoreException {
+import org.educoins.core.store.BlockStoreException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * The HttpServer serving {@link Block}s.
+ * Manages all ingoing requests to the REST-API.
+ * Created by typus on 11/5/15.
+ */
+@SpringBootApplication
+@EnableWebMvc
+@EnableAutoConfiguration(exclude = {JacksonAutoConfiguration.class})
+@ComponentScan(basePackages = "org.educoins.core")
+@ConfigurationProperties(value = "classpath:/application.properties")
+public class FullBlockChainNode {
+
+    public static void main(String[] args) throws BlockStoreException {
 //        ConfigurableApplicationContext run = SpringApplication.run(FullBlockChainNode.class, args);
 //        BlockChain blockChain = (BlockChain) run.getBean("blockChain");
 //        FullBlockChainPeer peer = new FullBlockChainPeer(blockChain);
@@ -37,5 +38,5 @@
 //        } catch (DiscoveryException e) {
 //            SpringApplication.exit(run, () -> -1);
 //        }
-//    }
-//}
+    }
+}
