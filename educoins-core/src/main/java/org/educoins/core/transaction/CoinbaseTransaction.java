1956b5777eef088b8e0ec0dd4040a1bfb0f547be
typusOmega <licksalt@gmx.net>
2016-01-14 11:42:30 +0100

    + Fixed potential Miner threading issue + CoinbaseTransaction does now only contain one output

--- educoins-core/src/main/java/org/educoins/core/transaction/CoinbaseTransaction.java
+++ educoins-core/src/main/java/org/educoins/core/transaction/CoinbaseTransaction.java
@@ -1,32 +1,24 @@
 package org.educoins.core.transaction;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.educoins.core.Wallet;
-import org.jetbrains.annotations.NotNull;
 
 public class CoinbaseTransaction extends Transaction {
 
-	public int amount;
-	public String publicKey;
-	
-	public CoinbaseTransaction(int amount, String publicKey) {
-		this.amount = amount;
-		this.publicKey = publicKey;
-	}
+    public int amount;
+    public String publicKey;
 
-	@Override
-	public Transaction create() {
-		
-		//Each output coins only a single coin.
-		//That later we do not have to deal with odd number of coins and reverse transactions.
-		for(int i = 0 ; i < amount ; i++){
-			Output out = new Output(1, publicKey);
-			super.addOutput(out);
-		}
-		return this;
-	}
+    public CoinbaseTransaction(int amount, String publicKey) {
+        this.amount = amount;
+        this.publicKey = publicKey;
+    }
+
+    @Override
+    public Transaction create() {
+        Output out = new Output(1, publicKey);
+        super.setOutputs(new ArrayList<Output>() {{
+            add(out);
+        }});
+        return this;
+    }
 
 }
