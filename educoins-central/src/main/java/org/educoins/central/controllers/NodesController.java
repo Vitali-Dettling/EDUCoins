affac246f7a633d1d90bd9d9519766e3d0016427
TypusOmega <licksalt@gmx.net>
2016-01-18 18:30:49 +0100

    + Central saving operation is now on read-commited transaction level

--- educoins-central/src/main/java/org/educoins/central/controllers/NodesController.java
+++ educoins-central/src/main/java/org/educoins/central/controllers/NodesController.java
@@ -7,7 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
@@ -42,9 +45,7 @@ public class NodesController {
 
         //TODO: get protocol aware
         node.setInetAddress(URI.create("http://" + inetAddr.toString() + ':' + node.getPort()));
-        synchronized (nodesRepository) {
-            nodesRepository.save(node);
-        }
+        nodesRepository.save(node);
 
         return new ResponseEntity<>(inetAddr, HttpStatus.OK);
     }
